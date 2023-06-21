0cc77baa970f83abc949a65394b0681e729a7795
saraiin <saraennaciri96@gmail.com>
2023-06-21 16:24:41 -0700

    tokens

---
+++ fortokens.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * freeTokens - Frees the global op_toks array of strings.
+ */
+void freeTokens(void)
+{
+	size_t i = 0;
+
+	if (op_toks == NULL)
+		return;
+
+	for (i = 0; op_toks[i]; i++)
+		free(op_toks[i]);
+
+	free(op_toks);
+}
+/**
+ * token_arr_len - Gets the length of current op_toks.
+ *
+ * Return: Length of current op_toks (as int).
+ */
+unsigned int token_arr_len(void)
+{
+	unsigned int toks_len = 0;
+
+	while (op_toks[toks_len])
+		toks_len++;
+	return (toks_len);
+}
