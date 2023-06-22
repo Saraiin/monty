7811e9406a6e9f61c0a0f11db2e8c11e9f87120c
samad <abdessamad.bouchelga@gmail.com>
2023-06-21 17:09:35 -0700

    errors

--- forerrors.c
+++ forerrors.c
@@ -1,16 +1,5 @@
 #include "monty.h"
 /**
- * openFileError - Prints file opening error messages with file name.
- * @filename: Name of file failed to open
- *
- * Return: (EXIT_FAILURE) always.
- */
-int openFileError(char *filename)
-{
-	fprintf(stderr, "Error: Can't open file %s\n", filename);
-	return (EXIT_FAILURE);
-}
-/**
  * openFileError - Prints file opening error messages w/ file name.
  * @filename: Name of file failed to open
  *
