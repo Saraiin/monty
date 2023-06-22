2d07c631ab0777e5b9250a3640a20dcab79f9496
saraiin <saraennaciri96@gmail.com>
2023-06-21 17:44:14 -0700

    pint

---
+++ mypint.c
@@ -0,0 +1,16 @@
+#include "monty.c"
+/**
+ * my_pint - Prints the top value of a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void my_pint(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL)
+	{
+		set_op_tok_error(pintError(line_number));
+		return;
+	}
+
+	printf("%d\n", (*stack)->next->n);
+}
