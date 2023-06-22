b837c5b1c9cb936d4b9475a99280d1b76abbd7db
saraiin <saraennaciri96@gmail.com>
2023-06-21 17:07:12 -0700

    op errors

--- operrors.c
+++ operrors.c
@@ -12,4 +12,48 @@ int unknownOpError(char *opcode, unsigned int line_number)
 		line_number, opcode);
 	return (EXIT_FAILURE);
 }
+/**
+ * no_int_error - Prints invalid monty_push argument error messages.
+ * @line_number: Line number in Monty bytecodes file where error occurred.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int no_int_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: usage: push integer\n", line_number);
+	return (EXIT_FAILURE);
+}
+/**
+ * set_op_tok_error - Sets last element of op_toks to be an error code.
+ * @error_code: Integer to store as a string in op_toks.
+ */
+void set_op_tok_error(int error_code)
+{
+	int toks_len = 0, i = 0;
+	char *exit_str = NULL;
+	char **new_toks = NULL;
 
+	toks_len = token_arr_len();
+	new_toks = malloc(sizeof(char *) * (toks_len + 2));
+	if (!op_toks)
+	{
+		malloc_error();
+		return;
+	}
+	while (i < toks_len)
+	{
+		new_toks[i] = op_toks[i];
+		i++;
+	}
+	exit_str = get_int(error_code);
+	if (!exit_str)
+	{
+		free(new_toks);
+		malloc_error();
+		return;
+	}
+	new_toks[i++] = exit_str;
+	new_toks[i] = NULL;
+	free(op_toks);
+	op_toks = new_toks;
+}
