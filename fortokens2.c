8781417af38c6d7b6890e31ff035c69b65013ce3
samad <abdessamad.bouchelga@gmail.com>
2023-06-22 03:40:59 -0700

    tokens

---
+++ fortokens2.c
@@ -0,0 +1,80 @@
+#include "monty.h"
+/**
+ * get_word_length - gets the word length of cur word in str
+ *
+ * @str: string to get word length from current word
+ * @delims: delimitors to use to delimit words
+ *
+ * Return: word length of current word
+ */
+
+int get_word_length(char *str, char *delims)
+{
+	int wLen = 0, pending = 1, i = 0;
+
+	while (*(str + i))
+	{
+		if (is_delim(str[i], delims))
+			pending = 1;
+		else if (pending)
+		{
+			wLen++;
+		}
+		if (wLen > 0 && is_delim(str[i], delims))
+			break;
+		i++;
+	}
+	return (wLen);
+}
+
+/**
+ * get_word_count - gets the word count of a string
+ *
+ * @str: string to get word count from
+ * @delims: delimitors to use to delimit words
+ *
+ * Return: the word count of the string
+ */
+
+int get_word_count(char *str, char *delims)
+{
+	int wc = 0, pending = 1, i = 0;
+
+	while (*(str + i))
+	{
+		if (is_delim(str[i], delims))
+			pending = 1;
+		else if (pending)
+		{
+			pending = 0;
+			wc++;
+		}
+		i++;
+	}
+	return (wc);
+}
+
+/**
+ * get_next_word - gets the next word in a string
+ *
+ * @str: string to get next word from
+ * @delims: delimitors to use to delimit words
+ *
+ * Return: pointer to first char of next word
+ */
+
+char *get_next_word(char *str, char *delims)
+{
+	int pending = 0;
+	int i = 0;
+
+	while (*(str + i))
+	{
+		if (is_delim(str[i], delims))
+			pending = 1;
+		else if (pending)
+			break;
+		i++;
+	}
+	return (str + i);
+}
