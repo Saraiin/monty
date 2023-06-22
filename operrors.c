b939e139869bb65749a7f6e4ff689bfb7b5ffa6c
saraiin <saraennaciri96@gmail.com>
2023-06-21 18:34:07 -0700

    nop

--- operrors.c
+++ operrors.c
@@ -57,3 +57,16 @@ void set_op_tok_error(int error_code)
 	free(op_toks);
 	op_toks = new_toks;
 }
+/**
+ * short_stack_error - Prints monty math function error messages
+ *                     for stacks/queues smaller than two nodes.
+ * @line_number: Line number in Monty bytecodes file where error occurred.
+ * @op: Operation where the error occurred.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int short_stack_error(unsigned int line_number, char *op)
+{
+	fprintf(stderr, "L%u: can't %s, stack too short\n", line_number, op);
+	return (EXIT_FAILURE);
+}
